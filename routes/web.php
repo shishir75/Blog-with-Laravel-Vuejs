7a1095cd0dde18d714bf7b211469912de1b3665d
shishir75 <shishir.srdr16@gmail.com>
2020-09-04 16:52:27 +0600

    Configure Frontend Index Page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get( '/', function () {
-    return view( 'welcome' );
+    return view( 'public.index' );
 } );
 
 Auth::routes();
